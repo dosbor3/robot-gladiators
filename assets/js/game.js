94abf132798a7c03aa45723bd545a5a6c9e27827
I am Billie <dosbor3@gmail.com>
2021-12-01 17:36:52 -0600

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -63,33 +63,70 @@ var fight = function(enemyName) {
     }    
 }
 
-//  * Fight all enemy-robots
-for (let i = 0; i < enemyNames.length; i++){
-    if (playerHealth > 0) {
-        //Inform the player of the current round number
-        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
 
-        //Pick new enemy to fight based on the index of the enemyNames array
-        var pickedEnemyName = enemyNames[i];
+//loop funcition for continuous play
+var startGame = function() {
+    //  * Fight all enemy-robots
+    for (var i = 0; i < enemyNames.length; i++) {
+        //reset player stats
+        playerHealth = 100;
+        playerAttack = 10;
+        playerMoney = 10;
         
-        //reset enemyHealh before starting new fight
-        enemyHealth = 50;
+        if (playerHealth > 0) {
+            //Inform the player of the current round number
+            window.alert("Welcome to Robot Gladiators " + playerName + "! Round " + (i + 1));
 
-        //use debugger to pause script from running and cheeck what's going on 
-        //debugger;
+            //Pick new enemy to fight based on the index of the enemyNames array
+            var pickedEnemyName = enemyNames[i];
+            
+            //reset enemyHealh before starting new fight
+            enemyHealth = 50;
 
-        //pass the pickedEnemyName variable's value into the fight function 
-        fight(enemyNames[i]);
-    }
+            //use debugger to pause script from running and cheeck what's going on 
+            //debugger;
+
+            //pass the pickedEnemyName variable's value into the fight function 
+            fight(enemyNames[i]);
+        }
     else {
-        window.alert("You have lost your robot in battle! Gam Over!");
+        window.alert(playerName + ", you have lost your robot in battle! Game Over!");
         break;
+        }
     }
+    endGame();    
 }
 
-//Game States
-//"WIN" - Player robot has defeated all enemy-robots
+//Start the game when the page loads
+startGame();
 
-//  * Defeate each enemy-robot
-// "LOSE" - Player robot's health is zero or less
+//provide game stats to player and option to end game
+var endGame = function() {
+    //if player is still alive, player wins!
+    if (playerHealth > 0){
+        window.alert("Great job, you've suvived he game! You now have a score pf " + playerMoney + ".");
+
+    }
+    else {
+        window.alert("You've lost your robot in battle.");
+    }
+
+    //Prompting the player for game continuation
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    //if player chooses to continue play, restart the game
+    if (playAgainConfirm) {
+        //restart the game
+        startGame();
+    }
+
+    else {
+       window.alert("The game has now ended. Let's se how you did!"); 
+    }
+
+    
+
+}
+
+
 
