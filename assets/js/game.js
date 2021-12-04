c96109062766b0a2a29446ebfe4e6d61477a14c6
I am Billie <dosbor3@gmail.com>
2021-12-04 16:52:35 -0600

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -33,8 +33,7 @@ var fight = function(enemyName) {
                 console.log("playerMoney", playerMoney);
                 break;
             }
-        }
-        
+        }        
         // remove enemy's health by subtracting the amount set in the playerAttack variable
         enemyHealth = enemyHealth - playerAttack;
         console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining." );  
@@ -63,7 +62,6 @@ var fight = function(enemyName) {
     }    
 }
 
-
 //loop funcition for continuous play
 var startGame = function() {
     //  * Fight all enemy-robots
@@ -87,19 +85,26 @@ var startGame = function() {
             //debugger;
 
             //pass the pickedEnemyName variable's value into the fight function 
-            fight(enemyNames[i]);
+            fight(pickedEnemyName); 
+
+            //if we're not at the last enemy in the array, and player has not been defeated
+            if (playerHealth > 0 && i < enemyNames.length - 1) {
+                //ask if player wants to use the store before next round
+                var storeConfirm = window.confirm("The fight is over, visit the store before the next round?")
+
+                if (storeConfirm){
+                    shop();
+                }                
+            }            
         }
+        
     else {
         window.alert(playerName + ", you have lost your robot in battle! Game Over!");
         break;
         }
     }
-    endGame();    
+       
 }
-
-//Start the game when the page loads
-startGame();
-
 //provide game stats to player and option to end game
 var endGame = function() {
     //if player is still alive, player wins!
@@ -123,10 +128,64 @@ var endGame = function() {
     else {
        window.alert("The game has now ended. Let's se how you did!"); 
     }
-
-    
-
 }
+
+//shop function that provides players with an option to retrieve additional perks
+var shop = function() {
+    //ask player what would they like to do
+    var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store?  Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice ");
+    
+        //Using switch to determine to REFILL, UPGRADE, or, LEAVE the shop based on shopOptionPrompt value
+        switch (shopOptionPrompt) {
+            case "REFILL":
+            case "refill":
+                if (playerMoney >= 7) {
+                    window.alert("Refilling player's health by 20 for $7 dollars.");
+
+                    //increase health and decrease money by updating playerHealth and playerMoney
+                    playerHealth += 20;
+                    playerMoney -= 7;
+                    break;
+                }
+                else {
+                    window.alert("You don't have enough money!");
+                }
+
+                case "UPGRADE":
+                case "upgrade" :
+                if (playerMoney >= 7){
+                    window.alert("Upgrading player's attack by 6 for $7 dollars.");
+                    
+                    //increase player's attack and decrease money by updating playerAttack and playerMoney
+                    playerAttack += 6;
+                    playerMoney -= 7;
+                    break;
+                }
+                else {
+                    window.alert("You don't have enough money!");
+                }
+
+                case "LEAVE":
+                case "leave":
+                window.alert("Leaving the store.");
+
+                //do nothing, so function will end
+                break;
+
+            default:
+                window.alert("You did not pick a valid option.  Try again.");
+                //call shop() again to force player to pick a valid option
+                shop();
+                break;            
+        }
+    }   
+
+//Start the game when the page loads
+startGame();
+
+//endGame function call
+ endGame();
 
 
 
